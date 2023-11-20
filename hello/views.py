704fa28f8603fde8e95da62cf7a39f81d341ec15
Jaseh Juropong <62422555+dmsrud015@users.noreply.github.com>
2023-11-20 16:51:21 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User 
+from django.shortcuts import render, redirect 
+
 
 # Create your views here.
 
@@ -18,7 +22,20 @@ def mypage(request):
     return render(request,'mypage.html')
     
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                                            name=request.POST['name'],
+                                            id=request.POST['id'],
+                                            password=request.POST['password1'],
+                                            email=request.POST['email'],)
+                                            
+            auth.login(request, user)
+            return redirect('')
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+    
+	
     
 def find_pw(request):
     return render(request,'find_pw.html')
