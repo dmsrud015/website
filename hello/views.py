9fcef9981be0746f81ee7dffad8889184e5fce5c
Jaseh Juropong <62422555+dmsrud015@users.noreply.github.com>
2023-11-22 16:23:56 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User 
-from django.shortcuts import render, redirect 
-
-
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from hello.forms import UserForm
 # Create your views here.
 
 def index(request):
@@ -22,19 +19,19 @@ def mypage(request):
     return render(request,'mypage.html')
     
 def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                                            name=request.POST['name'],
-                                            id=request.POST['id'],
-                                            password=request.POST['password1'],
-                                            email=request.POST['email'],)
-                                            
-            auth.login(request, user)
-            return redirect('')
-        return render(request, 'signup.html')
-    return render(request, 'signup.html')
-    
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            id = form.cleaned_data.get('id')
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(name=name,username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'hello/signup.html', {'form': form})
 	
     
 def find_pw(request):
