777660f7c193fdc5e83bc7d8c827258d9847eb02
Jaseh Juropong <62422555+dmsrud015@users.noreply.github.com>
2023-11-22 16:26:10 +0900

    Update urls.py

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("hello.urls"))
+    path('hello',include("hello.urls")),
+    path('', views.index, name='index'),
 ]
