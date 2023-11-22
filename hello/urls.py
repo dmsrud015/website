c911124355d8fd51ff5d3f4d30f5a114f26e7b84
Jaseh Juropong <62422555+dmsrud015@users.noreply.github.com>
2023-11-22 16:23:39 +0900

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'hello'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('buy', views.buy, name='buy'),
     path('collect', views.collect, name='collect'),
-    path('login', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='hello/login.html'), name='login'),
     path('mypage', views.mypage, name='mypage'),
     path('signup', views.signup, name='signup'),
     path('find_pw', views.find_pw, name='find_pw'),
