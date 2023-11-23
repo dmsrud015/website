84c869b0658d3c732317c9ca3bc696eceae225a1
Jaseh Juropong <62422555+dmsrud015@users.noreply.github.com>
2023-11-23 16:03:07 +0900

    Update settings.py

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,3 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "dmsrud015973@gmail.com"
+EMAIL_HOST_PASSWORD = "amau ctqm zdii joag"
+EMAIL_USE_TLS = True
+
+
